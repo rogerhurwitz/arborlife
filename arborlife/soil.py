1eb88aa7e880e665bb9b868bf16e0b538e9c46b2
Roger Hurwitz <rogerhurwitz@gmail.com>
2019-11-26 13:25:10 -0800

    Replaced magic numbers with constants

--- arborlife/soil.py
+++ arborlife/soil.py
@@ -19,19 +19,21 @@ class Soil:
     """
 
     FC_MOISTURE_FT3 = 1.18 * 10e26
+    MIN_FC, MAX_FC = 0, 10
 
     # TODO: Close w/ Neil on whether we need enhanced precision/accuracy
     # FC_MOISTURE_FT3 = Decimal("1.18") * Decimal("10e26")
 
-    def __init__(self, field_capacity=10):
+    def __init__(self, field_capacity=MAX_FC):
 
-        if field_capacity < 0 or field_capacity > 10:
-            raise ValueError("field_capacity must be between 0 and 10.")
+        if field_capacity < Soil.MIN_FC or field_capacity > Soil.MAX_FC:
+            raise ValueError(f"field_capacity must be between {Soil.MIN_FC}"
+                             f" and {Soil.MAX_FC}.")
 
         self._field_capacity = field_capacity
 
         # TODO: Close w/ Neil on allowing specified moisture initialization
-        self._moisture = field_capacity / 10 * Soil.FC_MOISTURE_FT3
+        self._moisture = field_capacity / Soil.MAX_FC * Soil.FC_MOISTURE_FT3
 
     @property
     def field_capacity(self):
@@ -47,7 +49,8 @@ class Soil:
 
     @moisture.setter
     def moisture(self, value):
-        max_moisture = self._field_capacity / 10 * Soil.FC_MOISTURE_FT3
+        max_moisture = (self._field_capacity / Soil.MAX_FC *
+                        Soil.FC_MOISTURE_FT3)
 
         if value > max_moisture or value < 0:
             raise ValueError(f"Attempt to set moisture below 0 or above"
