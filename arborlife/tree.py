4f61639c04f2b9f633fe1175995c7a69832e91a9
Roger Hurwitz <rogerhurwitz@gmail.com>
2019-12-13 11:19:52 -0800

    Added additional tree properties based on legacy code formulas and constants

--- arborlife/tree.py
+++ arborlife/tree.py
@@ -1,13 +1,19 @@
 import numpy as np
-from scipy import stats
 
 from arborlife import config, utils
 
-MAX_LEAF_CANOPY_PCT = .2
-GWT_DBH_H_COEFFICENT = 0.38315
-GWT_DBH_H_EXPONENT = 0.92045
+CANOPY_MASS_MAX = 99.0
+CANOPY_MASS_MEAN = 60.0
+CANOPY_MASS_MIN = 1.0
+CANOPY_MASS_SD = 10.0
 DBH_H_B0 = 3.84
 DBH_H_B1 = 19.66
+DBH_H_COEFFICENT = 0.38315
+DBH_H_EXPONENT = 0.92045
+HEIGHT_WIDTH_RATIO = 0.9
+MAX_LEAF_CANOPY_PCT = 0.2
+ROOT_CANOPY_PCT_MEAN = 0.60
+WOOD_DENSITY_LB = 45.0
 
 
 class Tree:
@@ -19,32 +25,24 @@ class Tree:
             arborlife.yml config file.
         alive (bool): True if tree is alive else false.  Initial state of
             tree set with tree alive value in arborlife.yml
-        canopy_mass (float): TBD
     """
 
     def __init__(self, age=None):
         tree_cfg = config.cfg["tree"]
 
-        self.age = age if age is not None else (
-            self._calc_initial_age(
-                tree_cfg["age_init_min"],
-                tree_cfg["age_init_max"],
-                tree_cfg["age_init_mean"],
-                tree_cfg["age_init_sd"],
+        self.age = float(age) if age is not None else (
+            utils.calc_truncnorm(
+                mean=tree_cfg["age_init_mean"],
+                sd=tree_cfg["age_init_sd"],
+                clip_a=tree_cfg["age_init_min"],
+                clip_b=tree_cfg["age_init_max"],
             )
         )
+        # Trees don't shrink, but mass can, so need track max height
         self._height_max = 0
         # 10 y/o tree canopy mass = 60kg, +/- 50kg each year away, min 10kg
         self.canopy_mass = max(10, 50 * self.age - 440)
         self.alive = True
-
-    def _calc_initial_age(self, min_age, max_age, mean, sd):
-        # https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.truncnorm.html
-        clip_b = np.inf if max_age is None else max_age
-        clip_a = -np.inf if min_age is None else min_age
-        return stats.truncnorm.rvs(
-            a=(clip_a - mean) / sd, b=(clip_b - mean) / sd, loc=mean, scale=sd
-        )
 
     @property
     def green_weight(self):
@@ -52,14 +50,40 @@ class Tree:
 
     @property
     def trunk_diameter(self):
-        d2h = (self.green_weight / GWT_DBH_H_COEFFICENT) ** (1 / GWT_DBH_H_EXPONENT)
-        return utils.solve_cubic(DBH_H_B1 / 12, DBH_H_B0 / 12, 0, -d2h)
+        d2h = (self.green_weight / DBH_H_COEFFICENT) ** (1 / DBH_H_EXPONENT)
+        return utils.calc_cubic(DBH_H_B1 / 12, DBH_H_B0 / 12, 0, -d2h)
 
     @property
     def height(self):
-        self._height_max = max(self._height_max, (DBH_H_B0 + DBH_H_B1 * self.trunk_diameter) / 12)
+        self._height_max = max(
+            self._height_max, (DBH_H_B0 + DBH_H_B1 * self.trunk_diameter) / 12)
         return self._height_max
 
     @property
     def bark_ft2(self):
         return self.height * 2 * np.pi * ((self.trunk_diameter / 12) / 2)
+
+    @property
+    def canopy_width(self):
+        return HEIGHT_WIDTH_RATIO * self.height
+
+    @property
+    def root_mass(self):
+        return ROOT_CANOPY_PCT_MEAN * self.canopy_mass
+
+    @property
+    def root_radius(self):
+        return ((self.root_mass / self.canopy_mass) / ROOT_CANOPY_PCT_MEAN) * self.canopy_width
+
+    @property
+    def root_area(self):
+        return np.pi * self.root_radius ** 2
+
+    @property
+    def root_ft3(self):
+        return self.root_mass / WOOD_DENSITY_LB
+
+    # @property
+    # def canopy_mass(self):
+    #     return utils.calc_truncnorm(
+    #         CANOPY_MASS_MEAN, CANOPY_MASS_SD, CANOPY_MASS_MIN, CANOPY_MASS_MAX)
--- arborlife/utils.py
+++ arborlife/utils.py
@@ -1,19 +1,29 @@
 import math
+import numpy as np
+import scipy.stats as stats
 
 
-def solve_cubic(a, b, c, d):
+def calc_cubic(a, b, c, d):
     p = -b / (3 * a)
     q = math.pow(p, 3) + (b * c - 3 * a * d) / (6 * math.pow(a, 2))
     r = c / (3 * a)
 
     x = (
         math.pow(
-            q + (math.pow(math.pow(q, 2) + (math.pow(r - math.pow(p, 2), 3)), 0.5)), 1 / 3,
+            q + (math.pow(math.pow(q, 2) + (math.pow(r - math.pow(p, 2), 3)), 0.5)), 1 / 3,     # noqa: E501
         )
         + math.pow(
-            q - (math.pow(math.pow(q, 2) + (math.pow(r - math.pow(p, 2), 3)), 0.5)), 1 / 3,
+            q - (math.pow(math.pow(q, 2) + (math.pow(r - math.pow(p, 2), 3)), 0.5)), 1 / 3,     # noqa: E501
         )
         + p
     )
-
     return x
+
+
+def calc_truncnorm(mean, sd, *, clip_a=-np.inf, clip_b=np.inf):
+    """Returns a float in a normal distribution (mean, sd) clipped (clip_a, clip_b)."""
+
+    # https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.truncnorm.html
+    return stats.truncnorm.rvs(
+        a=(clip_a - mean) / sd, b=(clip_b - mean) / sd, loc=mean, scale=sd
+    )
